f9827e611c27d2e95d66a8966a62f779f6a188ff
Lucas Amaral <42688542+LucasAmaral42@users.noreply.github.com>
2022-09-27 14:45:41 -0300

    add abstract_factory (#3)

---
+++ lib/creationals/abstract_factory/classes/knight.ex
@@ -0,0 +1,6 @@
+defmodule Creationals.AbstractFactory.Classes.Knight do
+  @moduledoc """
+  Module responsible to struct Knight class.
+  """
+  defstruct [:health, :armor, :intelligence, :dexterity, :strength]
+end
---
+++ lib/creationals/abstract_factory/classes/mage.ex
@@ -0,0 +1,6 @@
+defmodule Creationals.AbstractFactory.Classes.Mage do
+  @moduledoc """
+  Module responsible to struct Mage class.
+  """
+  defstruct [:health, :armor, :intelligence, :dexterity, :strength]
+end
